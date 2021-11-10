c49ef77a60b68a36e7875049675fd401a50a8a2a
FreewheelingHuman <93078433+FreewheelingHuman@users.noreply.github.com>
2021-11-10 10:48:49 -0500

    Refactor more of the settings module to be private

--- resources/lib/settings.py
+++ resources/lib/settings.py
@@ -12,16 +12,16 @@ class Settings:
 
         self.addon_id: str = self._addon.getAddonInfo('id')
 
-        self.manual = self.Manual(self._addon)
-        self.start = self.Start(self._addon)
-        self.in_progress = self.InProgress(self._addon)
-        self.state = self.State(self._addon)
+        self.manual = self._Manual(self._addon)
+        self.start = self._Start(self._addon)
+        self.in_progress = self._InProgress(self._addon)
+        self.state = self._State(self._addon)
 
-    class SubGroup:
+    class _SubGroup:
         def __init__(self, addon: xbmcaddon.Addon):
             self._addon = addon
 
-    class ActionGroup(SubGroup):
+    class _ActionGroup(_SubGroup):
         _clean = None
         _scan = None
 
@@ -41,15 +41,15 @@ class Settings:
         def scan(self, value: bool) -> None:
             self._addon.setSettingBool(self._scan, value)
 
-    class Manual(ActionGroup):
+    class _Manual(_ActionGroup):
         _clean = 'manual.clean'
         _scan = 'manual.scan'
 
-    class Start(ActionGroup):
+    class _Start(_ActionGroup):
         _clean = 'on_start.clean'
         _scan = 'on_start.scan'
 
-    class InProgress(ActionGroup):
+    class _InProgress(_ActionGroup):
         _active = 'in_progress.active'
         _scan = 'in_progress.scan'
 
@@ -69,7 +69,7 @@ class Settings:
         def clean(self, value: bool):
             raise RuntimeError('Clean value cannot be set for the in_progress action group.')
 
-    class State(SubGroup):
+    class _State(_SubGroup):
         _last_scan = 'state.last_scan'
 
         @property
