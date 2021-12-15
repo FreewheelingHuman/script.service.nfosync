1878824991e27bb1d9ad3c3711a4fba74ec8802e
FreewheelingHuman <93078433+FreewheelingHuman@users.noreply.github.com>
2021-12-15 10:17:19 -0500

    Add ability to run NFO Sync actions from other add-ons
    Closes #12

--- script.py
+++ script.py
@@ -1,9 +1,62 @@
+import sys
+
 import resources.lib.jsonrpc as jsonrpc
 from resources.lib.addon import addon
 
 
-jsonrpc.request(
-    'JSONRPC.NotifyAll',
-    sender=addon.id,
-    message=jsonrpc.INTERNAL_METHODS.immediate_sync.send,
-)
+def sync(arguments: list):
+    del arguments
+    jsonrpc.notify(message=jsonrpc.INTERNAL_METHODS.immediate_sync.send)
+
+
+def sync_one(arguments: list):
+    if len(arguments) < 2:
+        addon.log('Script - sync_one is missing arguments. Requires: media type, library id')
+        addon.notify(32074)
+        return
+
+    jsonrpc.notify(
+        message=jsonrpc.INTERNAL_METHODS.sync_one.send,
+        data={'type': arguments[0], 'id': arguments[1]}
+    )
+
+
+def export(arguments: list):
+    if len(arguments) < 2:
+        addon.log('Script - export is missing arguments. Requires: media type, library id')
+        addon.notify(32074)
+        return
+
+    jsonrpc.notify(
+        message=jsonrpc.INTERNAL_METHODS.export.send,
+        data={'type': arguments[0], 'id': arguments[1]}
+    )
+
+
+def export_all(arguments: list):
+    del arguments
+    jsonrpc.notify(message=jsonrpc.INTERNAL_METHODS.export_all.send)
+
+
+def import_all(arguments: list):
+    del arguments
+    jsonrpc.notify(message=jsonrpc.INTERNAL_METHODS.import_all.send)
+
+
+functions = {
+    'sync': sync,
+    'sync_one': sync_one,
+    'export': export,
+    'export_all': export_all,
+    'import_all': import_all
+}
+
+addon.log(f'Script - Running with parameters: {sys.argv}')
+
+command = 'sync'
+try:
+    command = sys.argv[1]
+except IndexError:
+    pass
+
+functions[command](sys.argv[2:])
